0b20d91aff78b1d4996fa2b341ed1c639e08a14f
martynasstogisktu <martynas.stogis@ktu.edu>
2023-04-01 17:18:50 +0300

    Initial push

---
+++ PMTS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PMTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PMTS/Models/PSQLcontext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PMTS.Models
+{
+    public class PSQLcontext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=PMTS;Username=postgres;Password=VG$2zcF1&kLXS@JFaY");
+        public PSQLcontext(DbContextOptions<PSQLcontext> options) 
+            : base(options)
+        { }
+    }
+
+    public class User
+    {
+        public int Id { get; set;}
+        public string Name { get; set;}
+        public string Email { get; set;}
+        public string Password { get; set;}
+    }
+}
